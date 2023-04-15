4fa285b20b6176256b5d633125064235f505e35c
BenjaminDanker <silvergem95@gmail.com>
2023-04-15 09:47:02 -0500

    Created login

--- server/server.js
+++ server/server.js
@@ -1,32 +1,62 @@
 ﻿const sql_organize = require("./sql_organize");
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(express.static("views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "unknownsecret",
+    //cookie: { secure: true }
+}))
 app.listen(3000);
 
 let data = null;
 const email = "123";
 const server_password = "123";
 
+
+app.get("/", function (req, res) {
+    if (req.session.user === undefined) {
+        res.render("index.ejs", { loggedin: false });
+    }
+    else {
+        res.render("index.ejs", { loggedin: true });
+    }
+});
+
 app.post("/addKanji", (req, res) => {
     data = req.body;
-    console.log(data);
-    res.send("Received");
 
     if (data.email === email && data.password === server_password) {
-        let val = sql_organize.insertKanjiSQL();
+        sql_organize.insertKanjiSQL();
     }
 });
 
 app.post("/addSignup", (req, res) => {
     data = req.body;
 
-    let val = sql_organize.insertUserPass(data);
+    sql_organize.insertEmailPass(data);
 
-    res.redirect("/index")
+    req.session.user = data.email;
+
+    res.redirect("/login.html");
 })
 
-sql_organize.variousSQL();
+app.post("/addLogin", (req, res) => {
+    data = req.body;
+    console.log("data", data);
+
+    sql_organize.getEmailPass(data).then(function (result) {
+        req.session.user = result[0].email;
+        res.redirect("..");
+    }).catch((err) => setImmediate(() => { throw err; }));;
+})
+
+//sql_organize.variousSQL();
--- server/sql_organize.js
+++ server/sql_organize.js
@@ -1,66 +1,67 @@
 const mysql = require("mysql");
 
-const sqlHost = "localhost";
-const sqlUser = "root";
-const sqlPassword = "password";
+
+function makeConnection() {
+    const con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "kanjiList"
+    });
+    con.connect((err) => {
+        if (err) console.log(err);
+        console.log("Connection Established");
+    })
+
+    return con;
+}
 
 module.exports = {
     insertKanjiSQL: function () {
-        const con = mysql.createConnection({
-            host: sqlHost,
-            user: sqlUser,
-            password: sqlPassword
-        });
-        con.connect((err) => {
-            if (err) console.log(err);
-            console.log("Connection Established");
-        })
-        con.query("USE kanjiList");
-        //con.query(`INSERT INTO memory (kanji, meaning, reading, stage, lastReviewTime) VALUES ('${data.kanjiToBeReviewed}', '${data.meaningToBeReviewed}', '${data.readingToBeReviewed}', 0, new Date().getTime());`);
-        con.query("SELECT * FROM memory", function (err, result) {
-            if (err) console.log(err);
-            console.log(result);
-        })
-        console.log(new Date().getTime());
+        let con = makeConnection();
 
-        con.end();
+        try {
+            con.query(`INSERT INTO memory (kanji, meaning, reading, stage, lastReviewTime) VALUES ('${data.kanjiToBeReviewed}', '${data.meaningToBeReviewed}', '${data.readingToBeReviewed}', 0, new Date().getTime());`);
+            con.query("SELECT * FROM memory", function (err, result) {
+                if (err) console.log(err);
+                console.log(result);
+            })
+        }
+        finally {
+            con.end();
+        }
     },
 
-    insertUserPass: function (data) {
-        const con = mysql.createConnection({
-            host: sqlHost,
-            user: sqlUser,
-            password: sqlPassword
+    insertEmailPass: function (data) {
+        let con = makeConnection();
+
+        try {
+            con.query(`INSERT INTO emailPass (email, password) VALUES ('${data.email}', '${data.password}');`);
+        }
+        finally {
+            con.end();
+        }
+    },
+
+    getEmailPass: function (data) {
+        return new Promise(function (resolve, reject) {
+            let con = makeConnection();
+
+            con.query(`SELECT * FROM emailPass WHERE email LIKE '%${data.email}%'`, function (err, result) {
+                if (err) console.log(err);
+                return resolve(result)
+            })
         });
-        con.connect((err) => {
-            if (err) console.log(err);
-            console.log("Connection Established");
-        })
-
-        con.query("USE kanjiList");
-        con.query(`INSERT INTO userPass (username, password) VALUES ('${data.username}', '${data.password}';`);
-        con.query("SELECT * FROM userPass", function (err, result) {
-            if (err) console.log(err);
-            console.log(result);
-        })
-
-        con.end();
     },
 
     variousSQL: function () {
-        const con = mysql.createConnection({
-            host: sqlHost,
-            user: sqlUser,
-            password: sqlPassword
-        });
-        con.connect((err) => {
-            if (err) console.log(err);
-        })
-        console.log("Connection Established");
-        con.query("USE kanjiList");
-        con.query("CREATE TABLE userPass (ID int AUTO_INCREMENT, primary key(ID), username varchar(30), password varchar(30);")
-        con.query("CREATE TABLE memory (ID int AUTO_INCREMENT, primary key(ID), kanji varchar(50), meaning varchar(255), reading varchar(50), stage int, latestReviewTime int);")
-        //con.query("DROP TABLE memory");
+        let con = makeConnection();
+
+        //con.query("CREATE TABLE emailPass (ID int AUTO_INCREMENT, primary key(ID), email varchar(30), password varchar(30));")
+        //con.query("CREATE TABLE memory (ID int AUTO_INCREMENT, primary key(ID), kanji varchar(50), meaning varchar(255), reading varchar(50), stage int, latestReviewTime int);")
+        //con.query("DROP TABLE userPass");
+        //con.query("DELETE FROM emailPass);
+        //con.query("SELECT * FROM emailPass", function (err, result) {if (err) console.log(err); console.log(result);})
 
         con.end();
     }
