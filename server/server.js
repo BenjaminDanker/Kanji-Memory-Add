4a15cca83ccc0807d89f5cc3a05b06d9e4df43ca
BenjaminDanker <silvergem95@gmail.com>
2023-04-26 18:36:52 -0500

    Moved getting review list code to util_functions file

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 ﻿const sql_organize = require("./sql_organize");
+const util_functions = require("./util_functions")
 const express = require("express");
 const session = require("express-session");
 
@@ -87,16 +88,7 @@ function isLoggedin(req, res, next) {
 // Handle review page
 app.get("/review", isLoggedin, function (req, res) {
     sql_organize.getVocab(req.session.userID).then(function (vocabList) {
-      // put only vocab past review due date into new list
-        currentTime = new Date().getTime();
-        let reviewList = [];
-        for (let i = 0; i < vocabList.length; i++) {
-            // if current vocab stage less than max (7), and current time is greater than next review time
-            if (vocabList[i].stage < 7 && vocabList[i].nextReviewTime < currentTime) {
-                reviewList.push(vocabList[i]);
-            }
-        }
-      //
+        let reviewList = util_functions.getReviewList(vocabList);
 
         if (reviewList.length > 0) {
             // add 'end' as an indicator to stop review
---
+++ server/util_functions.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // Put only vocab past review due date into new list
+    getReviewList: function (vocabList) {
+        currentTime = new Date().getTime();
+        let reviewList = [];
+
+        for (let i = 0; i < vocabList.length; i++) {
+            // if current vocab stage less than max (7), and current time is greater than next review time
+            if (vocabList[i].stage < 7 && vocabList[i].nextReviewTime < currentTime) {
+                reviewList.push(vocabList[i]);
+            }
+        }
+
+        return reviewList
+    }
+}
