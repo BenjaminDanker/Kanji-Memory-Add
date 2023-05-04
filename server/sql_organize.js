31405b4569864f7f5aceeaabc5768918ace15b36
BenjaminDanker <silvergem95@gmail.com>
2023-05-04 03:21:19 -0500

    Change to using mysql2/promise with await Previously, new Promise() was used. Similarly, mysql does not support await. Since mysql2 supports await, the connection will be made before querying.
    For mysql2 = require("mysql2"), it is separate for the express-session package's internal code.

--- server/sql_organize.js
+++ server/sql_organize.js
@@ -1,4 +1,5 @@
-﻿const mysql = require("mysql");
+﻿const mysql2 = require("mysql2");
+const mysql = require("mysql2/promise");
 const session = require("express-session");
 const MySQLStore = require("express-mysql-session")(session);
 
@@ -31,10 +32,7 @@ const options = {
 // create mySQL connection 
 function makeConnection() {
     const con = mysql.createConnection(options);
-    con.connect((err) => {
-        if (err) console.log(err);
-        console.log("Connection Established");
-    });
+    console.log("Connection Established")
 
     return con;
 }
@@ -42,22 +40,22 @@ function makeConnection() {
 module.exports = {
     // get msqyl connection for session storage
     getSessionConnection: function () {
-        const con = mysql.createConnection(options);
+        const con = mysql2.createConnection(options);
         const sessionStore = new MySQLStore({}, con);
 
         return sessionStore;
     },
 
     // Put vocabulary info into database from userscript
-    insertVocab: function (data, userID) {
-        let con = makeConnection();
+    insertVocab: async function (data, userID) {
+        let con = await makeConnection();
 
         try {
             currentTime = new Date().getTime();
             const fourhours = 1000 * 60 * 60 * 4;
             nextReviewTime = currentTime + fourhours;
 
-            con.query(`INSERT INTO vocab (parent_ID, kanji, meaning, reading, stage, nextReviewTime) VALUES ("${userID}", "${data.kanjiToBeReviewed}", "${data.meaningToBeReviewed}", "${data.readingToBeReviewed}", 0, ${nextReviewTime});`);
+            await con.query(`INSERT INTO vocab (parent_ID, kanji, meaning, reading, stage, nextReviewTime) VALUES ("${userID}", "${data.kanjiToBeReviewed}", "${data.meaningToBeReviewed}", "${data.readingToBeReviewed}", 0, ${nextReviewTime});`);
         }
         finally {
             con.end();
@@ -65,25 +63,21 @@ module.exports = {
     },
 
     // Get vocab
-    getVocab: function (userID) {
-        return new Promise(function (resolve, reject) {
-            let con = makeConnection();
+    getVocab: async function (userID) {
+        let con = await makeConnection();
 
-            try {
-                con.query(`SELECT * FROM vocab WHERE parent_ID LIKE ${userID}`, function (err, vocabList) {
-                    if (err) console.log(err);
-                    return resolve(vocabList);
-                });
-            }
-            finally {
-                con.end();
-            }
-        });
+        try {
+            let [result, fields] = await con.query(`SELECT * FROM vocab WHERE parent_ID LIKE ${userID}`);
+            return result
+        }
+        finally {
+            con.end();
+        }
     },
 
     // Updates database at review end
-    updateVocab: function (checkIfList, reviewList) {
-        let con = makeConnection();
+    updateVocab: async function (checkIfList, reviewList) {
+        let con = await makeConnection();
 
         try {
             for (let i = 0; i < checkIfList.length; i++) {
@@ -97,7 +91,7 @@ module.exports = {
 
                     reviewList[i].stage += 1;
 
-                    con.query(`UPDATE vocab SET stage=${reviewList[i].stage}, nextReviewTime=${reviewList[i].nextReviewTime} WHERE vocabID=${reviewList[i].vocabID}`);
+                    await con.query(`UPDATE vocab SET stage=${reviewList[i].stage}, nextReviewTime=${reviewList[i].nextReviewTime} WHERE vocabID=${reviewList[i].vocabID}`);
                 }
             }
         }
@@ -107,11 +101,11 @@ module.exports = {
     },
 
     // Put email and password into database from signup
-    insertUserInfo: function (data) {
-        let con = makeConnection();
+    insertUserInfo: async function (data) {
+        let con = await makeConnection();
 
         try {
-            con.query(`INSERT INTO userInfo (username, email, password) VALUES ('${data.username}', '${data.email}', '${data.password}');`);
+            await con.query(`INSERT INTO userInfo (username, email, password) VALUES ('${data.username}', '${data.email}', '${data.password}');`);
         }
         finally {
             con.end();
@@ -119,36 +113,32 @@ module.exports = {
     },
 
     // Get email and password
-    getUserInfo: function (data) {
-        // uses Promise to wait for query, then return result
-        return new Promise(function (resolve, reject) {
-            let con = makeConnection();
+    getUserInfo: async function (data) {
+        let con = await makeConnection();
 
-            try {
-                con.query(`SELECT * FROM userInfo WHERE email LIKE '${data.email}'`, function (err, result) {
-                    if (err) console.log(err);
-                    return resolve(result);
-                });
-            }
-            finally {
-                con.end();
-            }
-        });
+        try {
+            let [result, fields] = await con.query(`SELECT * FROM userInfo WHERE email LIKE '${data.email}'`)
+            return result
+        }
+        finally {
+            con.end();
+        }
     },
 
     // Miscellaneous mySQL usage
-    variousSQL: function () {
-        let con = makeConnection();
+    variousSQL: async function () {
+        let con = await makeConnection();
 
         //con.query("CREATE TABLE userInfo (userID int AUTO_INCREMENT, primary key(userID), username varchar(50), email varchar(30), password varchar(30))")
         //con.query("CREATE TABLE vocab (parent_ID int, INDEX index_parent_ID (parent_ID), FOREIGN KEY (parent_ID) REFERENCES userInfo(userID) ON UPDATE CASCADE ON DELETE CASCADE, vocabID int AUTO_INCREMENT, primary key(vocabID), kanji varchar(50), meaning varchar(1000), reading varchar(50), stage int, nextReviewTime bigint)")
         //con.query("DROP TABLE vocab");
         //con.query("DROP TABLE userInfo");
         //con.query(`INSERT INTO test (kanji, meaning, reading, stage, latestReviewTime) VALUES ('${data.kanjiToBeReviewed}', '${data.meaningToBeReviewed}', '${data.readingToBeReviewed}', 0, ${timeOfReview});`);
-        con.query(`SELECT * FROM vocab WHERE parent_ID LIKE 2`, function (err, result) { if (err) console.log(err); console.log(result); })
-        //con.query("SELECT * FROM userInfo", function (err, result) { if (err) console.log(err); console.log(result); })
-        //con.query("SELECT * FROM sessions", function (err, result) { if (err) console.log(err); console.log(result); })
-        //con.query("DELETE FROM vocab");
+        //con.query(`SELECT * FROM vocab WHERE parent_ID LIKE 2`, function (err, result) { if (err) console.log(err); console.log(result); })
+        //let [result, fields] = await con.query("SELECT * FROM userInfo")
+        let [result, fields] = await con.query("SELECT * FROM sessions")
+        //con.query("DELETE FROM userInfo");
+        console.log(result)
         con.end();
     }
 }
