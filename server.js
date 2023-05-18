99ba02f6c76c459d14b68739fb8893c2fcbf47ad
BenjaminDanker <silvergem95@gmail.com>
2023-05-18 09:48:49 -0500

    Revert "Set cookies security false"
    This reverts commit 9f9fdb6009a71e02535822f1a54067ee3fb967a4.

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(RateLimit({
 app.use(session({
     secret: "unknownsecret",
     store: sql_organize.getSessionConnection(),
-    cookie: { secure: false, maxAge: 500000000000 },
+    cookie: { secure: true, maxAge: 500000000000 },
     resave: false,
     saveUninitialized: false
 }));
