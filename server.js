050fd406487bed75ba50a56c697793cf915a372c
BenjaminDanker <silvergem95@gmail.com>
2023-05-23 13:50:30 -0500

    Add req and res log to checkForVocab

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.listen(process.env.PORT || 3000);
 // Handle request to check for vocabulary in database
 app.post("/checkForVocab", async function (req, res) {
     let data = req.body;
-    console.log("/checkForVocab data: ", data)
+    console.log("/checkForVocab req", req)
+    console.log("/checkForVocab res", res)
     // get user information stored in database
     let userInfo = await sql_organize.getUserInfo(data);
 
