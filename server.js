1f79e74ce30fb614b497fa80225e5e4cb3a9a2cd
BenjaminDanker <silvergem95@gmail.com>
2023-05-23 19:50:24 -0500

    Change log to data

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.listen(process.env.PORT || 3000);
 // Handle request to check for vocabulary in database
 app.post("/checkForVocab", async function (req, res) {
     let data = req.body;
-    console.log("/checkForVocab res", res)
+    console.log("/checkForVocab res", data)
     // get user information stored in database
     let userInfo = await sql_organize.getUserInfo(data);
 
